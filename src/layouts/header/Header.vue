1340eeb6cff412535a8cd3ee554fd23d5704a9e1
hongyu.yan <hongyu.yan@nio.com>
2024-01-31 15:05:29 +0800

    feat:build basic page

---
+++ src/layouts/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="height: 100%; display: flex; align-items: center;">
+        <img style="padding-right: 10px;" src="/vite.svg">
+        <el-text class="mx-1" type="primary" size="large">Aahyyyyy</el-text>
+    </div>
+</template>
+
+<script lang="ts">
+    import './Header.scss'
+    export default {
+        name: "Header"
+    }
+</script>
